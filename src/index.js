b8b0b8f9bfc966fa3e9c4cb30383e06125b5e387
Marculis <78306008+Marculis@users.noreply.github.com>
2021-03-15 23:33:17 +0300

    feat: edit code
    edit code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-exports.min = function min (array) {
-  return Math.min (null, array);
-}
+exports.min = function min(array) {
+    return Math.min.apply(null, array);
+};
 
-exports.max = function max (array) {
-  return Math.max(null, array);
-}
+exports.max = function max(array) {
+    return Math.max.apply(null, array);
+};
 
-exports.avg = function avg (array) {
-let sum = (accumulator, currentValue) => accumulator + currentValue;
-return (sum / array.length);
-}
+exports.avg = function avg(array) {
+    return array.reduce((a, b) => a + b) / array.length;
+};
